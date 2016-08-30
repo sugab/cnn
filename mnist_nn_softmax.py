7991c6f90518417ed362af37f586dac739866331
Bagus Cahyono <baguscah77@gmail.com>
2016-08-30 10:52:47 +0700

    Softmax DONE Successfully

--- mnist_nn_softmax.py
+++ mnist_nn_softmax.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Aug 28 12:52:58 2016
+Created on Sun Aug 29 12:52:58 2016
 
 @author: baguscahyono
 """
@@ -9,22 +9,22 @@ import numpy as np
 import mnist
 import cnn
 
-tr_data, tr_label = mnist.load_mnist(path="", selection=slice(0, 100), digits=[0,1])
-vl_data, vl_label = mnist.load_mnist(path="", selection=slice(100, 200), digits=[0,1])
+tr_data, tr_label = mnist.load_mnist(path="", selection=slice(0, 2000))
+vl_data, vl_label = mnist.load_mnist(path="", selection=slice(2000, 2500))
 
 #%% INIIT FILTER
 
 np.random.seed(1)
 
 l0 = tr_data.reshape((tr_data.shape[0], 784))
-lb = tr_label[:, np.newaxis]
+lb = tr_label
 
 syn0 = np.random.normal(0, 0.01, (784, 196))
 bias_0 = np.zeros((1, 196))
 syn1 = np.random.normal(0, 0.01, (196, 49))
 bias_1 = np.zeros((1, 49))
-syn2 = np.random.normal(0, 0.01, (49, 1))
-bias_2 = np.zeros((1, 1))
+syn2 = np.random.normal(0, 0.01, (49, 10))
+bias_2 = np.zeros((1, 10))
 
 lsyn0 = 0
 lsyn1 = 0
@@ -32,21 +32,22 @@ lsyn2 = 0
 
 #%% TRAINING
 
-lr = 0.01
-momentum = 0.9
+lr = 0.005
+momentum = 0.5
 
-for i in xrange(60):
+for i in xrange(5000):
     
     l1 = cnn.sigmoid(np.dot(l0, syn0) + bias_0)
     l2 = cnn.sigmoid(np.dot(l1, syn1) + bias_1)
-    l3 = cnn.sigmoid(np.dot(l2, syn2) + bias_2)
-
-    e = l3 - lb
+    l3_n = np.dot(l2, syn2) + bias_2
     
-#    print "%s" % (i,)
-#    print "%s" % (l3)
+    exp_scores = np.exp(l3_n)
+    l3 = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
 
-    ld3 = e * cnn.sigmoid(l3, deriv=True)
+    result = np.argmax(l3, axis=1)
+        
+    ld3 = l3
+    ld3[range(len(lb)), lb] -= 1
     ld2 = np.dot(ld3, syn2.T) * cnn.relu(l2, deriv=True)
     ld1 = np.dot(ld2, syn1.T) * cnn.relu(l1, deriv=True)
     
@@ -68,12 +69,11 @@ for i in xrange(60):
 
 
 #%% CALCULATE ACCURACY
+cft = np.zeros((10, 10))
+for j in xrange(len(lb)):
+    cft[result[j], lb[j]] += 1
     
-cf = np.zeros((2, 2))
-for i in xrange(lb.shape[0]):
-    cf[int(round(lb[i])), int(round(l3[i]))] += 1
-
-print "Accuracy Training: %f" % ((cf[0,0] + cf[1,1]) / float(lb.shape[0]))
+print "EPOCH: %s = %s" % (i, np.sum(np.diag(cft)))
 
 l0 = vl_data.reshape((vl_data.shape[0], 784))
 lb = vl_label[:, np.newaxis]
@@ -82,8 +82,9 @@ l1 = cnn.sigmoid(np.dot(l0, syn0) + bias_0)
 l2 = cnn.sigmoid(np.dot(l1, syn1) + bias_1)
 l3 = cnn.sigmoid(np.dot(l2, syn2) + bias_2)
 
-cf = np.zeros((2, 2))
-for i in xrange(lb.shape[0]):
-    cf[int(round(lb[i])), int(round(l3[i]))] += 1
+result = np.argmax(l3, axis=1)
+cfv = np.zeros((10, 10))
+for i in xrange(len(lb)):
+    cfv[result[i], lb[i]] += 1
 
-print "Accuracy Validation: %f" % ((cf[0,0] + cf[1,1]) / float(lb.shape[0]))
+print "Validation Accuracy: %s" % np.sum(np.diag(cfv))
