d4aca9c085ff06219a02110168f0f8307d7e3f52
Bagus Cahyono <baguscah77@gmail.com>
2016-12-05 14:57:24 +0700

    Fixing miss naming and miss code

--- tubes_relu_2.py
+++ tubes_relu_2.py
@@ -5,13 +5,13 @@ import cnn2 as c
 
 np.random.seed(2)
 
-l1 = c.ConvolutionLayer(h.random((16, 1, 5, 5)), np.ones((16, 1)), p=2, activation=c.ELULayer)
+l1 = c.ConvolutionLayer(h.random((16, 1, 5, 5)), np.ones((16, 1)), p=2, activation=c.ReLULayer)
 l2 = c.PoolingLayer(2, 2)
-l3 = c.ConvolutionLayer(h.random((32, 16, 3, 3)), np.ones((32, 1)), activation=c.ELULayer)
+l3 = c.ConvolutionLayer(h.random((32, 16, 3, 3)), np.ones((32, 1)), activation=c.ReLULayer)
 l4 = c.PoolingLayer(2, 2)
-l5 = c.ConvolutionLayer(h.random((32, 32, 3, 3)), np.ones((32, 1)), p=1, activation=c.ELULayer)
+l5 = c.ConvolutionLayer(h.random((32, 32, 3, 3)), np.ones((32, 1)), p=1, activation=c.ReLULayer)
 l6 = c.PoolingLayer(2, 2)
-l7 = c.FCLayer(h.random((288, 64)), np.ones(64), activation=c.ELULayer)
+l7 = c.FCLayer(h.random((288, 64)), np.ones(64), activation=c.ReLULayer)
 l8 = c.FCLayer(h.random((64, 10)), np.ones(10))
 
 model = [l1, l2, l3, l4, l5, l6]
@@ -27,5 +27,5 @@ data = [tr_data, tr_label, vl_data, vl_label]
 network = c.CNN(model, c.SoftmaxLayer, data, lr=0.00005, momentum=0.9, dropout=0.8)
 t, v = network.run(500, 25)
 
-np.save('tubes_elu_2_t', t)
-np.save('tubes_elu_2_v', v)
+np.save('tubes_relu_2_t', t)
+np.save('tubes_relu_2_v', v)
--- tubes_relu_3.py
+++ tubes_relu_3.py
@@ -5,13 +5,13 @@ import cnn2 as c
 
 np.random.seed(2)
 
-l1 = c.ConvolutionLayer(h.random((16, 1, 5, 5)), np.ones((16, 1)), p=2, activation=c.LeakyReLULayer)
+l1 = c.ConvolutionLayer(h.random((16, 1, 5, 5)), np.ones((16, 1)), p=2, activation=c.ReLULayer)
 l2 = c.PoolingLayer(2, 2)
-l3 = c.ConvolutionLayer(h.random((32, 16, 3, 3)), np.ones((32, 1)), activation=c.LeakyReLULayer)
+l3 = c.ConvolutionLayer(h.random((32, 16, 3, 3)), np.ones((32, 1)), activation=c.ReLULayer)
 l4 = c.PoolingLayer(2, 2)
-l5 = c.ConvolutionLayer(h.random((32, 32, 3, 3)), np.ones((32, 1)), p=1, activation=c.LeakyReLULayer)
+l5 = c.ConvolutionLayer(h.random((32, 32, 3, 3)), np.ones((32, 1)), p=1, activation=c.ReLULayer)
 l6 = c.PoolingLayer(2, 2)
-l7 = c.FCLayer(h.random((288, 64)), np.ones(64), activation=c.LeakyReLULayer)
+l7 = c.FCLayer(h.random((288, 64)), np.ones(64), activation=c.ReLULayer)
 l8 = c.FCLayer(h.random((64, 10)), np.ones(10))
 
 model = [l1, l2, l3, l4, l5, l6]
@@ -27,5 +27,5 @@ data = [tr_data, tr_label, vl_data, vl_label]
 network = c.CNN(model, c.SoftmaxLayer, data, lr=0.00001, momentum=0.9, dropout=0.8)
 t, v = network.run(500, 25)
 
-np.save('tubes_lrelu_3_t', t)
-np.save('tubes_lrelu_3_v', v)
+np.save('tubes_relu_3_t', t)
+np.save('tubes_relu_3_v', v)
